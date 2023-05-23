69876054daf6fa7e93cd19216c3cbf297723e4d9
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-05-23 12:19:37 +0200

    added base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import ssr from 'vite-plugin-ssr/plugin';
 import { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [react(), ssr({prerender:true})],
+	plugins: [react(), ssr({ prerender: true })],
+	base: '/Manny-Website/',
 	ssr: {
 		noExternal: ['magicscroll','slick-carousel','react-slick', 'enquire.js']
 	}
